84753ebc774b2f9bdb1f4e906e80fff00e5aa407
Denilsonrsl <denilsonr228778@gmail.com>
2026-05-17 21:26:18 -0300

    Delete docs/src/main directory
    Limpando arquivos duplicados

--- docs/src/main/java/com/labreserve/labreserve/Main.java
+++
@@ -1,8 +0,0 @@
-package com.labreserve.labreserve;
-
-public class Main {
-    public static void main(String[] args) {
-        // Isso desvia a exigência de módulos do JavaFX e inicia o seu App!
-        App.main(args);
-    }
-}
